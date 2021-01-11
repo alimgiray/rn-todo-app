cdb38923c49e4d76a658b49dd22e5e7c043542a6
Alim Giray Aytar <alimgiray@gmail.com>
2021-01-11 20:55:22 +0300

    init

--- App.js
+++ App.js
@@ -1,21 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, View, FlatList, Button } from 'react-native';
+
+import GoalItem from './components/GoalItem';
+import GoalInput from './components/GoalInput';
 
 export default function App() {
+  const [courseGoals, setCourseGoals] = useState([]);
+  const addGoalHandler = (goal) => {
+    setCourseGoals(currentGoals => [...currentGoals, { id: Math.random().toString(), value: goal}]);
+  };
+  const deleteGoalHandler = (id) => {
+    setCourseGoals(currentGoals => [...currentGoals.filter((goal) => goal.id !== id)])
+  }
+  const [modalState, setModalState] = useState(false);
+  const setModalStateHandler = () => {
+    setModalState(currentModalState => !currentModalState);
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.screen}>
+      <Button title="Add new" onPress={setModalStateHandler} />
+      <GoalInput onAddGoal={addGoalHandler} modalState={modalState} setModalState={setModalStateHandler} />
+      <FlatList 
+        data={courseGoals} 
+        keyExtractor={(item, index) => item.id}
+        renderItem={goal => <GoalItem title={goal.item.value} id={goal.item.id} onDelete={deleteGoalHandler} />} 
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  screen: {
+    paddingTop: 50,
+    paddingHorizontal: 10
   },
 });
